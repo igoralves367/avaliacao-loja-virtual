f56d0277add1fa0c3bc5ebf5239f7d8ec1553f0d
igoralves367 <igor.alves367@gmail.com>
2023-08-02 20:16:36 -0300

    Finaliza metodo criar produto, na camda de InfraRepository e SpringDataJPA

--- src/main/java/br/com/siteware/lojavirtual/produto/infra/ProdutoInfraRepository.java
+++ src/main/java/br/com/siteware/lojavirtual/produto/infra/ProdutoInfraRepository.java
@@ -4,15 +4,19 @@ import org.springframework.stereotype.Repository;
 
 import br.com.siteware.lojavirtual.produto.application.repository.ProdutoRepository;
 import br.com.siteware.lojavirtual.produto.domain.Produto;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 @Repository
 @Log4j2
+@RequiredArgsConstructor
 public class ProdutoInfraRepository implements ProdutoRepository {
+	private final ProdutoSpringDataJPARepository produtoSpringDataJPARepository; 
 
 	@Override
 	public Produto salva(Produto produto) {
 		log.info("[start] ProdutoInfraRepository - salva");
+		produtoSpringDataJPARepository.save(produto);
 		log.info("[finish] ProdutoInfraRepository - salva");
-		return null;
+		return produto;
 	}
 }
