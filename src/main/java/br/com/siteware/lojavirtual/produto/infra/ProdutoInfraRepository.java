d3089a1ea4b46ea6ecf2778e3e47408772dc44c8
igoralves367 <igor.alves367@gmail.com>
2023-08-02 21:13:47 -0300

    finaliza metodo consulta produto, implementando camada de infra e adiciona excessao em salvar produto e consulta produto

--- src/main/java/br/com/siteware/lojavirtual/produto/infra/ProdutoInfraRepository.java
+++ src/main/java/br/com/siteware/lojavirtual/produto/infra/ProdutoInfraRepository.java
@@ -3,8 +3,11 @@ package br.com.siteware.lojavirtual.produto.infra;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
+import br.com.siteware.lojavirtual.handler.APIException;
 import br.com.siteware.lojavirtual.produto.application.repository.ProdutoRepository;
 import br.com.siteware.lojavirtual.produto.domain.Produto;
 import lombok.RequiredArgsConstructor;
@@ -17,10 +20,17 @@ public class ProdutoInfraRepository implements ProdutoRepository {
 
 	@Override
 	public Produto salva(Produto produto) {
-		log.info("[start] ProdutoInfraRepository - salva");
-		produtoSpringDataJPARepository.save(produto);
-		log.info("[finish] ProdutoInfraRepository - salva");
-		return produto;
+	    log.info("[inicia] ProdutoInfraRepository - salva ");
+	    try {
+	        produtoSpringDataJPARepository.save(produto);
+	        log.info("[sucesso] ProdutoInfraRepository - salva: Produto cadastrado com sucesso: {}", produto.getIdProduto());
+	        return produto;
+	    } catch (DataIntegrityViolationException e) {
+	        log.error("[erro] ProdutoInfraRepository - salva: Erro ao salvar produto: {}", e.getMessage());
+	        throw APIException.build(HttpStatus.BAD_REQUEST, "Erro ao salvar produto. Verifique os dados informados.");
+	    } finally {
+	        log.info("[finaliza] ProdutoInfraRepository - salva ");
+	    }
 	}
 
 	@Override
@@ -34,7 +44,9 @@ public class ProdutoInfraRepository implements ProdutoRepository {
 	@Override
 	public Produto consultaProdutoAtravesId(UUID idProduto) {
 		log.info("[start] ProdutoInfraRepository - consultaProdutoAtravesId");
+		Produto produto = produtoSpringDataJPARepository.findById(idProduto)
+				.orElseThrow(() -> APIException.build(HttpStatus.NOT_FOUND, "Produto não encontrado"));
 		log.info("[finish] ProdutoInfraRepository - consultaProdutoAtravesId");
-		return null;
+		return produto;
 	}
 }
