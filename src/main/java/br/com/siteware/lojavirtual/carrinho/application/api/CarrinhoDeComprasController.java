b72d183e4fa27b54a1d29b94de8683b90c045c6a
igoralves367 <igor.alves367@gmail.com>
2023-08-03 23:01:09 -0300

    Modifica COntroller e Response

--- src/main/java/br/com/siteware/lojavirtual/carrinho/application/api/CarrinhoDeComprasController.java
+++ src/main/java/br/com/siteware/lojavirtual/carrinho/application/api/CarrinhoDeComprasController.java
@@ -3,15 +3,20 @@ package br.com.siteware.lojavirtual.carrinho.application.api;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.siteware.lojavirtual.carrinho.application.api.responses.CarrinhoResponse;
+import br.com.siteware.lojavirtual.carrinho.application.service.CarrinhoDeComprasService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 @Log4j2
 @RestController
+@RequiredArgsConstructor
 public class CarrinhoDeComprasController implements CarrinhoDeComprasAPI {
+	private final CarrinhoDeComprasService carrinhoDeComprasService; 
 
 	@Override
 	public CarrinhoResponse criarCarrinho() {
 		log.info("[start] CarrinhoDeComprasController - criaCarrinhoDeCompras");
+		CarrinhoResponse carrinhoResponse = carrinhoDeComprasService.criaCarrinhoDeCompras();
 		log.info("[finish] CarrinhoDeComprasController - criaCarrinhoDeCompras");
-		return null;
+		return carrinhoResponse;
 	}
 }
--- src/main/java/br/com/siteware/lojavirtual/carrinho/application/api/responses/CarrinhoResponse.java
+++ src/main/java/br/com/siteware/lojavirtual/carrinho/application/api/responses/CarrinhoResponse.java
@@ -1,6 +1,7 @@
 package br.com.siteware.lojavirtual.carrinho.application.api.responses;
 
 import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
@@ -8,7 +9,13 @@ import lombok.Value;
 
 @Value
 public class CarrinhoResponse {
-	private UUID id;
+	private UUID idCarrinhoDeCompras;
     private List<ItemCarrinhoResponse> itens;
     private BigDecimal valorTotal;
+
+    public CarrinhoResponse(UUID idCarrinhoDeCompras) {
+        this.idCarrinhoDeCompras = idCarrinhoDeCompras;
+        this.itens = Collections.emptyList();
+        this.valorTotal = BigDecimal.ZERO;
+    }
 }
